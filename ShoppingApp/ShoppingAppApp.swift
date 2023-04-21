0716074102ff11d32834d1e90c5f73d215acc224
Tolgahan Sönmez <tolgahansonmez@outlook.com.tr>
2023-04-21 16:14:27 +0300

    Initial Commit

---
+++ ShoppingApp/ShoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingAppApp.swift
+//  ShoppingApp
+//
+//  Created by tolgahan sonmez on 21.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
